385203b878b357183db2355a2c2d49fbf49f7650
Subhraprakash2020 <subhra.techy@gmail.com>
2025-11-20 22:30:17 +0530

    Fix spotless

--- src/main/java/com/healthcare/patient/exception/ResourceNotFoundException.java
+++ src/main/java/com/healthcare/patient/exception/ResourceNotFoundException.java
@@ -11,6 +11,6 @@ public class ResourceNotFoundException extends RuntimeException {
   }
 
   public ResourceNotFoundException(String message, Throwable throwable) {
-       super(message, throwable);
+    super(message, throwable);
   }
 }
