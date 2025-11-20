2be147d25ea536f257d949a453743a4286dc3b47
Subhraprakash2020 <subhra.techy@gmail.com>
2025-11-20 23:30:53 +0530

    Fix spotless

--- src/main/java/com/healthcare/patient/model/Patient.java
+++ src/main/java/com/healthcare/patient/model/Patient.java
@@ -18,7 +18,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "patients")
 public class Patient {
   @Transient public static final String SEQUENCE_NAME = "patients_sequence";
-  
+
   @Id private long id;
 
   @NotBlank
