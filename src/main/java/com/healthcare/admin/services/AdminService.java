f1277cee6b36bc257ba77be6d3f29fc8ea236e7e
Subhraprakash2020 <subhra.techy@gmail.com>
2025-12-04 21:31:56 +0530

    Fix spotless

--- src/main/java/com/healthcare/admin/services/AdminService.java
+++ src/main/java/com/healthcare/admin/services/AdminService.java
@@ -1,10 +1,9 @@
 package com.healthcare.admin.services;
 
 import com.healthcare.admin.model.Admin;
+import com.healthcare.patient.model.Patient;
 import java.util.List;
 import org.springframework.security.core.userdetails.UserDetails;
-import com.healthcare.patient.model.Patient;
-
 
 public interface AdminService {
   Admin createAdmin(Admin admin);
