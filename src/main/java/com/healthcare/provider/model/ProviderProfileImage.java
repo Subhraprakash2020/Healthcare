b61c01b7955ec9e96a4d894f0d27df94434a4aa5
Subhraprakash2020 <subhra.techy@gmail.com>
2025-12-16 23:31:30 +0530

    Clean code.

--- src/main/java/com/healthcare/provider/model/ProviderProfileImage.java
+++ src/main/java/com/healthcare/provider/model/ProviderProfileImage.java
@@ -18,48 +18,4 @@ public class ProviderProfileImage {
   private String providerId;
   private String imageUrl;
   private Date uploadedAt = new Date();
-
-  //   public String getproviderId() {
-  //     return providerId;
-  //   }
-
-  //   public void setId(String providerId) {
-  //     this.providerId = providerId;
-  //   }
-
-  //   public Long getPatientId() {
-  //     return patientId;
-  //   }
-
-  //   public void setPatientId(Long patientId) {
-  //     this.patientId = patientId;
-  //   }
-
-  //   public String getImageUrl() {
-  //     return imageUrl;
-  //   }
-
-  //   public void setImageUrl(String imageUrl) {
-  //     this.imageUrl = imageUrl;
-  //   }
-
-  //   public Date getUploadedAt() {
-  //     return uploadedAt;
-  //   }
-
-  //   public void setUploadedAt(Date uploadedAt) {
-  //     this.uploadedAt = uploadedAt;
-  //   }
-  // public ProviderProfileImage(String id, Long patientId, String imageUrl, Date uploadedAt) {
-  // 	super();
-  // 	this.id = id;
-  // 	this.patientId = patientId;
-  // 	this.imageUrl = imageUrl;
-  // 	this.uploadedAt = uploadedAt;
-  // }
-  // public ProviderProfileImage() {
-  // 	super();
-  // 	// TODO Auto-generated constructor stub
-  // }
-
 }
